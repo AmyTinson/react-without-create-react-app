502cc0ab20f5244a6cd989586d903312683871f8
Amy Tinson <amyelizabethtinson@gmail.com>
2021-07-26 19:27:44 -0400

    and we are up and running

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import App from './components/App'
+
+const appRouting = (
+  <Router>
+    <Switch>
+      <Route exact path='/' component={App} />
+    </Switch>
+  </Router>
+)
+
+ReactDOM.render(appRouting, document.getElementById('root'))
